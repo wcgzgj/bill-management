0b6092e4bcfb77812bf6198938c0025e1c7ab09d
wcgzgj <zhangjia_good@126.com>
2021-06-22 22:16:35 +0800

    配置 vue 部分配置

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 
+// 修改axios的baseUrl，这样axios就不用每次都在请求中填写 VUE_APP_SERVER  了
+axios.defaults.baseURL=process.env.VUE_APP_SERVER;
 
 const app = createApp(App)
     app.use(store)
